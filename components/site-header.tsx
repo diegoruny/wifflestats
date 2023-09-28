7485c8a22ff5317fd9f83c70e1b4abac5e511774
Diego Delgado <diegoruny@gmail.com>
2023-09-28 17:32:27 -0500

    comments added

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,9 +12,7 @@ export function SiteHeader() {
 	return (
 		<header className="bg-background sticky top-0 z-40 w-full border-b">
 			<div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-				<div className="sm:hidden">
-					<HamburgerNav />
-				</div>
+				<div className="sm:hidden">{/* <HamburgerNav /> */}</div>
 				<div className="hidden sm:inline-block">
 					<MainNav items={siteConfig.mainNav} />
 				</div>
